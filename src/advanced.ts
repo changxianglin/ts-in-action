377fcadea0fa26dd81cede3c6b6f7b314c7606b3
changxianglin <281532612@qq.com>
2020-09-23 01:06:58 +0800

    advanced

---
+++ src/advanced.ts
@@ -0,0 +1,18 @@
+let a = 1
+let b = [1, null]
+
+let c = (x = 1) => x + 1
+
+window.onkeydown = (event) => {
+  // console.log(event.button)
+}
+
+interface Foo {
+  bar: number
+}
+// let foo = {} as Foo
+// foo.bar = 1
+
+let foo: Foo = {
+  bar: 1,
+}
