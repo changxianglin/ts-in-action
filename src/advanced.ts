28f346fcb196bd30c74736e4080b96f056ce9820
changxianglin <281532612@qq.com>
2020-09-23 01:36:29 +0800

    advanced2

--- src/advanced.ts
+++ src/advanced.ts
@@ -3,9 +3,9 @@ let b = [1, null]
 
 let c = (x = 1) => x + 1
 
-window.onkeydown = (event) => {
+// window.onkeydown = (event) => {
   // console.log(event.button)
-}
+// }
 
 interface Foo {
   bar: number
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@
 // import './advanceClass'
 // import './class-interface'
 // import './genencs'
-import './genencs2'
+// import './genencs2'
+// import './advanced'
+import './advanced2'
 
 let hello: string = 'Hello Typescript!'
 
