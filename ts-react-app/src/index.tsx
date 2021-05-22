b93fb9cb4fb2ff76024629a895b5ab6c79f2e14b
changxianglin <281532612@qq.com>
2021-05-22 09:17:30 +0800

    remove project in project

--- ts-react-app/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import Hello from './components/demo/Hello'
-
-ReactDOM.render(
-  <Hello name="TypeScript!" />,
-  document.querySelectorAll('.app')[0]
-)
